8196e98a61a26eb640d5f4fa7c04c58a7b4f0f87
smriti999 <126649362+smriti999@users.noreply.github.com>
2025-12-08 21:31:04 +0545

    created auth and working on homepage

--- EducationalAdministrativSite/routes/web.php
+++ EducationalAdministrativSite/routes/web.php
@@ -3,6 +3,12 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+Route::view('/', 'home')->name('home');
+Route::view('/about', 'about')->name('about');
+Route::view('/courses', 'courses')->name('courses');
+Route::view('/gallery', 'gallery')->name('gallery');
+Route::view('/contact', 'contact')->name('contact');
+
 Route::get('/', function () {
     return view('welcome');
 });
